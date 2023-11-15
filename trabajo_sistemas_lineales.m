d935cfb3613dc255ef7dc508d6c19521b2334245
elizabeth-123 <92232518+elizabeth-123@users.noreply.github.com>
2023-11-15 17:19:43 -0500

    Update trabajo_sistemas_lineales.m

--- trabajo_sistemas_lineales.m
+++ trabajo_sistemas_lineales.m
@@ -19,6 +19,7 @@ for i=n1:b(1)-n1
         for k=1:n
             for l=1:n
             w = w+g_sigma(sqrt((-n1+k)^2+(-n1+l)^2),0,sigma_s)*g_sigma(imagen(i-n1+k,j-n1+l),imagen(i,j),sigma_r)*imagen(i-n1+k,j-n1+l);
+            %factor de normalización
             wp= wp+g_sigma(sqrt((-n1+k)^2+(-n1+l)^2),0,sigma_s)*g_sigma(imagen(i-n1+k,j-n1+l),imagen(i,j),sigma_r);
             end
         end
